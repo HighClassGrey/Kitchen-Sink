1f94651de55bcd658c30c52024fe54342875c983
HighClassGrey <tamekaahannah@gmail.com>
2018-09-19 14:39:47 -0500

    JS LAB

---
+++ app.js
@@ -0,0 +1,39 @@
+
+let fullName = 'Tameka Hannah';
+
+const usStates = '50';
+
+var compute = 5 + 4;
+
+let yourname = 'Angel';
+
+    //if the first letter of yourname is comes after L Back of line, if not Next
+if (yourname.charCodeAt(0) > '76')  {
+    console.log('Back of the line!');
+
+} else {
+    console.log('Next!');
+}
+
+
+    //shows error message
+function myFunction() {
+    alert('Hello World!');
+}
+
+
+    //check age function
+function checkAge(name, age) {
+if (age < 21) 
+     output = 'Sorry '+ name  + ' you are not old enough to view this page!';
+else     output = 'Thank You';
+return output
+}
+checkAge('charles', 21)
+checkAge('Abby', 21)
+checkAge('James', 18)
+checkAge('John', 17)
+
+
+var Veges = ['Broccoli', 'Green Beans', 'Cabbage']
+
